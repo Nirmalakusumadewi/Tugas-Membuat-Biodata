5466c78386fb7823809eac0421129849eb844525
Nirmalakusumadewi <164704409+Nirmalakusumadewi@users.noreply.github.com>
2024-03-27 11:21:11 +0800

    Add files via upload

---
+++ Biodataa.py
@@ -0,0 +1,14 @@
+nama =("Nirmala Kusuma Dewi")
+prodi =("Sistem Informasi")
+alamat =("Selebung Rembige Kec.Janapria")
+lulusan =("SMK ISLAM ADDUHA")
+usia =int("19")
+
+print ("Nama:",nama)
+print ("Jurusan:",prodi)
+print ("Alamat:",alamat)
+print ("Lulusan:",lulusan)
+print ("Umur:",usia)
+
+
+           
